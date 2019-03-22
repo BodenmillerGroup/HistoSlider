0b817bdde438eda007e4a185eb5ebb41d8e9c842
Anton Rau <anton.rau@gmail.com>
2019-03-22 16:29:20 +0100

    Streamlined histogram editor.

--- histoslider/ui/histogram_view.py
+++ histoslider/ui/histogram_view.py
@@ -5,8 +5,6 @@ from histoslider.ui.histogram_item import HistogramItem
 class HistogramView(HistogramItem):
     def __init__(self, image_item: ChannelImageItem, blend_view=None):
         HistogramItem.__init__(self, image_item, bounds=(image_item.channel.settings.min, image_item.channel.settings.max))
-        labelStyle = {'font-size': '10pt'}
-        self.axis.setLabel(image_item.channel.label, **labelStyle)
         self.blend_view = blend_view
         self.channel = image_item.channel
         self.setLevels(*self.channel.settings.levels)
